100c4b164b008be291ad172b1989bc3882d6816c
Aline Santos Lima <asantoli@everis.com>
2020-10-25 19:35:47 -0300

    retirando acentos

--- src/SomarDigitosNumeroInteiro.java
+++ src/SomarDigitosNumeroInteiro.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SomarDígitosNúmeroInteiro {
+public class SomarDigitosNumeroInteiro {
 	
 	public static void main(String[] args) {
 		Scanner leitor = new Scanner(System.in);
